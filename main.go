dada04b7e7bb41727e78de67e680232d583930ba
Das, Barun <bdas31@gatech.edu>
2023-11-21 16:40:38 -0500

    added env vars with godotenv

--- main.go
+++ main.go
@@ -1,7 +1,24 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
+)
 
 func main () {
-	fmt.Println("Hello world!")
+	// fmt.Println("Hello world!")
+	dotenv := loadEnvVars("CLIENT_ID")
+	fmt.Printf("godotenv: %s=%s \n", "CLIENT_ID", dotenv)
 }
+
+func loadEnvVars(key string) string {
+	err := godotenv.Load(".env")
+	if err != nil {
+		log.Fatalf("Error loading .env file")
+	}
+	return os.Getenv(key)
+}
+
